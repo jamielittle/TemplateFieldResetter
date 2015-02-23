977fbce2399001ca83c503d8dff0999a62d6fe48
jamie.little <jamie.little@calonwebsolutions.co.uk>
2015-02-23 13:36:00 +0000

    Initial Commit of project

---
+++ TemplateFieldResetter/Controllers/EntityController.cs
@@ -0,0 +1,21 @@
+﻿using TemplateFieldResetter.Models;
+using TemplateFieldResetter.Repositories;
+using Sitecore.Services.Core;
+using Sitecore.Services.Infrastructure.Sitecore.Services;
+
+namespace TemplateFieldResetter.Controllers
+{
+    [ServicesController]
+    public class EntityController : EntityService<Entity>
+    {
+        public EntityController(IRepository<Entity> repository)
+            : base(repository)
+        {
+        }
+
+        public EntityController()
+            : this(new EntityRespository())
+        {
+        }
+    }
+}
---
+++ TemplateFieldResetter/Models/Entity.cs
@@ -0,0 +1,6 @@
+﻿namespace TemplateFieldResetter.Models
+{
+    public class Entity : Sitecore.Services.Core.Model.EntityIdentity
+    {
+    }
+}
